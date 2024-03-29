dc4a4d9a41cd64367469a2896d8fdaf9c7b81f34
vishalfecund <vishalgarg@fecundservices.com>
2024-03-29 11:05:31 +0530

    Changed css attribute

--- hrportal/client/src/components/SignupForm.js
+++ hrportal/client/src/components/SignupForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import backgroundImage from "../assets/common-background-image.png";
 import popupBackground from "../assets/popup-background.png";
-import "./SignupForm.css";
+import styles from "./SignupForm.module.css";
 import SignupSuccessfully from "./SignupSuccessfully.js";
 
 const SignUpForm = () => {
@@ -50,15 +50,15 @@ const SignUpForm = () => {
 
   return (
     <div
-      className="signup-container"
+      className={styles.signup_container}
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
-      <form onSubmit={handleSubmit} className="signup-form">
-        <div className="title-container">
-          <p className="form-title">Sign up to Hiring Portal</p>
+      <form onSubmit={handleSubmit} className={styles.signup_form}>
+        <div className={styles.title_container}>
+          <p className={styles.form_title}>Sign up to Hiring Portal</p>
         </div>
-        <div className="sub-container">
-          <label htmlFor="EmployeeFirstName" className="label-type">
+        <div className={styles.sub_container}>
+          <label htmlFor="EmployeeFirstName" className={styles.label_type}>
             Employee First Name
           </label>
           <input
@@ -66,150 +66,150 @@ const SignUpForm = () => {
             value={firstName}
             // onChange={(e) => setEmployeeFirstName(e.target.value)}
             onChange={(e) => setFirstName(e.target.value)}
-            className="Signup-input"
+            className={styles.login_input}
             name="EmployeeFirstName"
             required
           />
         </div>
         <br />
-        <div className="sub-container">
-          <label htmlFor="EmployeeLastName" className="label-type">
+        <div className={styles.sub_container}>
+          <label htmlFor="EmployeeLastName" className={styles.label_type}>
             Employee Last Name
           </label>
           <input
             type="text"
             value={lastName}
             onChange={(e) => setLastName(e.target.value)}
-            className="Signup-input"
+            className={styles.login_input}
             name="EmployeeLastName"
             required
           />
         </div>
         <br />
-        <div className="sub-container">
-          <label htmlFor="EmployeeID" className="label-type">
+        <div className={styles.sub_container}>
+          <label htmlFor="EmployeeID" className={styles.label_type}>
             Employee ID
           </label>
           <input
             type="number"
             value={employeeId}
             onChange={(e) => setEmployeeId(e.target.value)}
-            className="Signup-input"
+            className={styles.login_input}
             name="EmployeeID"
             required
           />
         </div>
         <br />
-        <div className="sub-container">
-          <label htmlFor="email" className="label-type">
+        <div className={styles.sub_container}>
+          <label htmlFor="email" className={styles.label_type}>
             Email ID
           </label>
           <input
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            className="Signup-input"
+            className={styles.login_input}
             name="email"
             required
           />
         </div>
         <br />
-        <div className="sub-container">
-          <label htmlFor="password" className="label-type">
+        <div className={styles.sub_container}>
+          <label htmlFor="password" className={styles.label_type}>
             Password
           </label>
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className="login-input"
+            className={styles.login_input}
             name="password"
             required
           />
         </div>
         {/* <br /> */}
         <div>
-          <div className="sub-container-question">
-            <label className="label-type">Security Question 1</label>
-            <p htmlFor="SecurityQuestion1" className="p-type">
+          <div className={styles.sub_container_question}>
+            <label className={styles.label_type}>Security Question 1</label>
+            <p htmlFor="SecurityQuestion1" className={styles.p_type}>
               What is your first pet name ?
             </p>
           </div>
-          <div className="sub-container">
-            <label className="label-type">Answer</label>
+          <div className={styles.sub_container}>
+            <label className={styles.label_type}>Answer</label>
             <input
               type="text"
               value={answer1}
               // onChange={(e) => setEmployeeFirstName(e.target.value)}
               onChange={(e) => setAnswer1(e.target.value)}
-              className="Signup-input"
+              className={styles.login_input}
               name="SecurityQuestion1"
               required
             />
           </div>
         </div>
         <div>
-          <div className="sub-container-question">
-            <label className="label-type">Security Question 2</label>
-            <p htmlFor="SecurityQuestion1" className="p-type">
+          <div className={styles.sub_container_question}>
+            <label className={styles.label_type}>Security Question 2</label>
+            <p htmlFor="SecurityQuestion1" className={styles.p_type}>
               What was your childhood nickname ?
             </p>
           </div>
-          <div className="sub-container">
-            <label className="label-type">Answer</label>
+          <div className={styles.sub_container}>
+            <label className={styles.label_type}>Answer</label>
             <input
               type="text"
               value={answer2}
               // onChange={(e) => setEmployeeFirstName(e.target.value)}
               onChange={(e) => setAnswer2(e.target.value)}
-              className="Signup-input"
+              className={styles.login_input}
               name="SecurityQuestion2"
               required
             />
           </div>
         </div>
         <div>
-          <div className="sub-container-question">
-            <label className="label-type">Security Question 3</label>
-            <p htmlFor="SecurityQuestion3" className="p-type">
+          <div className={styles.sub_container_question}>
+            <label className={styles.label_type}>Security Question 3</label>
+            <p htmlFor="SecurityQuestion3" className={styles.p_type}>
               What was your first mobile brand name ?
             </p>
           </div>
-          <div className="sub-container">
-            <label className="label-type">Answer</label>
+          <div className={styles.sub_container}>
+            <label className={styles.label_type}>Answer</label>
             <input
               type="text"
               value={answer3}
               // onChange={(e) => setEmployeeFirstName(e.target.value)}
               onChange={(e) => setAnswer3(e.target.value)}
-              className="Signup-input"
+              className={styles.login_input}
               name="SecurityQuestion3"
               required
             />
           </div>
         </div>
         <br />
-        <div className="Signup-button-container">
+        <div className={styles.Signup_button_container}>
           <button
             type="button"
             onClick={handleCancel}
-            className="Signup-button"
+            className={styles.Signup_button}
           >
             Cancel
           </button>
           <button
             type="submit"
             onClick={() => setShowPopup(!showPopup)}
-            className="Signup-button"
+            className={styles.Signup_button}
           >
             Sign-up
           </button>
         </div>
       </form>
       {showPopup && (
-        <div className="popup" onClick={togglePopup}>
+        <div className={styles.popup} onClick={togglePopup}>
           <div
-            className="popup-content"
+            className={styles.popup_content}
             style={{ backgroundImage: `url(${popupBackground})` }}
           >
             <SignupSuccessfully
@@ -221,10 +221,10 @@ const SignUpForm = () => {
               answer2={answer2}
               answer3={answer3}
             />
-            <p className="popup-message">
+            <p className={styles.popup_message}>
               Form submitted successfully!<a href="/"> Login Here</a>
             </p>
-            <button className="popup-close-button" onClick={togglePopup}>
+            <button className={styles.popup_close_button} onClick={togglePopup}>
               Close
             </button>
           </div>
--- hrportal/client/src/components/SignupSuccessfully.js
+++ hrportal/client/src/components/SignupSuccessfully.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./SignupSuccessfully.css";
+import styles from "./SignupSuccessfully.module.css";
 // import backgroundImage from "../assets/common-background-image.png"
 
 const SignupSuccessfully = ({
@@ -12,34 +12,34 @@ const SignupSuccessfully = ({
   answer3,
 }) => {
   return (
-    <div className="readonly-fields" >
-      <div className="field-row">
-        <div className="field-label">First Name</div>
-        <div className="field-value">{firstName}</div>
+    <div className={styles.readonly_fields} >
+      <div className={styles.field_row}>
+        <div className={styles.field_label}>First Name</div>
+        <div className={styles.field_value}>{firstName}</div>
       </div>
-      <div className="field-row">
-        <div className="field-label">Last Name</div>
-        <div className="field-value">{lastName}</div>
+      <div className={styles.field_row}>
+        <div className={styles.field_label}>Last Name</div>
+        <div className={styles.field_value}>{lastName}</div>
       </div>
-      <div className="field-row">
-        <div className="field-label">Employee ID</div>
-        <div className="field-value">{employeeId}</div>
+      <div className={styles.field_row}>
+        <div className={styles.field_label}>Employee ID</div>
+        <div className={styles.field_value}>{employeeId}</div>
       </div>
-      <div className="field-row">
-        <div className="field-label">Email</div>
-        <div className="field-value">{email}</div>
+      <div className={styles.field_row}>
+        <div className={styles.field_label}>Email</div>
+        <div className={styles.field_value}>{email}</div>
       </div>
-      <div className="field-row">
-        <div className="field-label">What is your first pet name ?</div>
-        <div className="field-value">{answer1}</div>
+      <div className={styles.field_row}>
+        <div className={styles.field_label}>What is your first pet name ?</div>
+        <div className={styles.field_value}>{answer1}</div>
       </div>
-      <div className="field-row">
-        <div className="field-label">What was your childhood nickname ?</div>
-        <div className="field-value">{answer2}</div>
+      <div className={styles.field_row}>
+        <div className={styles.field_label}>What was your childhood nickname ?</div>
+        <div className={styles.field_value}>{answer2}</div>
       </div>
-      <div className="field-row">
-        <div className="field-label">What was your first mobile brand name ?</div>
-        <div className="field-value">{answer3}</div>
+      <div className={styles.field_row}>
+        <div className={styles.field_label}>What was your first mobile brand name ?</div>
+        <div className={styles.field_value}>{answer3}</div>
       </div>
     </div>
   );
