803e2e9afe15ed2be7cb1949b57847b964c20d45
vishalfecund <vishalgarg@fecundservices.com>
2024-04-10 19:42:52 +0530

    Reset password 2

---
+++ hrportal/client/src/components/ResetPassword.js
@@ -0,0 +1,180 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import backgroundImage from "../assets/common-background-image.png"; // Import the background image
+import styles from "./ResetPassword.module.css"; // Import the CSS file
+//import Login from './Login'; // Import the Login component
+
+const securityQuestions = [
+  {
+    value: "What is your first pet name?",
+    label: "What is your first pet name?",
+  },
+  {
+    value: "What was your childhood nickname?",
+    label: "What was your childhood nickname?",
+  },
+  {
+    value: "What was your first mobile brand name?",
+    label: "What was your first mobile brand name?",
+  },
+];
+
+function ResetPassword() {
+  const [employeeId, setEmployeeId] = useState("");
+  const [email, setEmail] = useState("");
+  const [securityQuestion, setSecurityQuestion] = useState(
+    securityQuestions[0].value
+  );
+  const [answer, setAnswer] = useState("");
+  const [isVerifying, setIsVerifying] = useState(false);
+  const [error, setError] = useState(null);
+  const [showPopup, setShowPopup] = useState(false);
+
+  const navigateToLogin = useNavigate();
+  // const navigateToPopup = useNavigate();
+
+  const handleSubmit = (event) => {
+    setIsVerifying(true);
+    event.preventDefault();
+
+    const userData = {
+      employeeId: employeeId,
+      email: email,
+      securityQuestion: securityQuestion,
+      answer: answer,
+    };
+
+    setShowPopup(true);
+
+    console.log(userData);
+    // Implement logic to reset password based on user input
+    console.log("Employee ID:", employeeId);
+    console.log("Email:", email);
+    console.log("Security Question:", securityQuestion);
+    console.log("Answer:", answer);
+    setIsVerifying(false);
+  };
+
+  const handleCancel = (e) => navigateToLogin("/");
+
+  const togglePopup = () => {
+    navigateToLogin("/");
+  };
+
+  return (
+    <div
+      className={styles.reset_container}
+      style={{
+        // backgroundImage: `url(${backgroundImage})`,
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <form onSubmit={handleSubmit} className={styles.reset_password_form}>
+        <h1 className={styles.reset_password_title}>Reset Password</h1>
+        <div className={styles.form_field}>
+          <label htmlFor="employeeId" className={styles.form_label}>
+            Employee ID:
+          </label>
+          <input
+            type="number"
+            id="employeeId"
+            value={employeeId}
+            onChange={(e) => setEmployeeId(e.target.value)}
+            required
+            className={styles.form_input}
+          />
+        </div>
+        <div className={styles.form_field}>
+          <label htmlFor="emailId" className={styles.form_label}>
+            Email ID:
+          </label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            className={styles.form_input}
+          />
+        </div>
+        <div className={styles.form_field}>
+          <label htmlFor="securityQuestion" className={styles.form_label}>
+            Security Question:
+          </label>
+          <select
+            id="securityQuestion"
+            value={securityQuestion}
+            onChange={(e) => setSecurityQuestion(e.target.value)}
+            required
+            className={styles.form_input}
+          >
+            {/* // <option value="">Select a question</option> */}
+            {securityQuestions.map((question) => (
+              <option key={question.value} value={question.value}>
+                {question.label}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className={styles.form_field}>
+          <label htmlFor="answer" className={styles.form_label}>
+            Answer:
+          </label>
+          <input
+            type="text"
+            id="answer"
+            value={answer}
+            onChange={(e) => setAnswer(e.target.value)}
+            required
+            className={styles.form_input}
+          />
+        </div>
+
+        {error && <p className={styles.errorMessage}>{error}</p>}
+
+        <div className={styles.Signup_button_container}>
+          <button
+            type="button"
+            onClick={handleCancel}
+            className={styles.Signup_button}
+          >
+            Cancel
+          </button>
+
+          <button
+            type="submit"
+            //onClick={() => setShowPopup(!showPopup)}
+            className={styles.Signup_button}
+            disabled={isVerifying}
+          >
+            {isVerifying ? "Verifying..." : "Verify"}
+          </button>
+        </div>
+      </form>
+      {showPopup && (
+        <div className={styles.popup} onClick={togglePopup}>
+          <div
+            className={styles.popup_content}
+            // style={{ backgroundImage: `url(${popupBackground})` }}
+          >
+            <p className={styles.popup_message}>
+              Form submitted successfully!
+              <br />
+              <a href="/" className={styles.login_here}>
+                {" "}
+                Login Here
+              </a>
+            </p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ResetPassword;
