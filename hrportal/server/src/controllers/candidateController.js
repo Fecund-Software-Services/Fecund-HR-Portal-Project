633b407e8f31abbf2eb7049f381ca1ef732954aa
unknown <sanjay.hs@fecundservices.com>
2024-04-17 12:50:05 +0530

    Feature: Updated Schema ofcandidate and Resume

---
+++ hrportal/server/src/controllers/candidateController.js
@@ -0,0 +1,20 @@
+/*
+Project: Hiring Portal Project
+Author: HS
+Date: 17/4/2024
+Sprint: Sprint 2
+
+Modification Log:
+-------------------------------------------------------------------------------------------------------
+Date        |   Author                  |   Sprint   |    Description 
+-------------------------------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------------------------------
+*/
+
+const Candidate = require("../collections/candidates");
+const Resume = require("../collections/resumes")
+
+{
+    // functions for candidate
+}
