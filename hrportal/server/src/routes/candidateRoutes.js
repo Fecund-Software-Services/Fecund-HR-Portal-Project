e005ded24f8aa2438b99ce539ecb6440b1b8debd
unknown <sanjay.hs@fecundservices.com>
2024-04-29 13:04:19 +0530

    Feature: Added Search Candidate functionality

--- hrportal/server/src/routes/candidateRoutes.js
+++ hrportal/server/src/routes/candidateRoutes.js
@@ -8,17 +8,20 @@ Modification Log:
 -------------------------------------------------------------------------------------------------------
 Date        |   Author                  |   Sprint   |    Description 
 -------------------------------------------------------------------------------------------------------
-
+29/4/2024           HS                      3                  Search candidate validation
 -------------------------------------------------------------------------------------------------------
 */
 
 const express = require('express')
-const { addCandidate, upload } = require('../controllers/candidateController')
+const { addCandidate, searchCandidate, upload } = require('../controllers/candidateController')
 
 const router = express.Router()
 
 // add new candidate
 router.post('/add-candidate', upload.single('resume'), addCandidate)
+
+// search for candidates
+router.get('/search-candidate', searchCandidate)
 
 // exporting router
 module.exports = router
