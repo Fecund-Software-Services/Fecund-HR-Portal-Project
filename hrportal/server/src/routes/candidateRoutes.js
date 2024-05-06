3468db4e3b6c6d0d8908ac04fc9cf5b296e72b3d
Harshini Chengappa <harshini.chengappa@fecundservices.com>
2024-05-06 13:27:11 +0530

    changed candidate schema and worked on view candidate details by year and month

--- hrportal/server/src/routes/candidateRoutes.js
+++ hrportal/server/src/routes/candidateRoutes.js
@@ -8,15 +8,15 @@ Modification Log:
 -------------------------------------------------------------------------------------------------------
 Date        | Author                  | Sprint   | Description 
 -------------------------------------------------------------------------------------------------------
-29/4/2024           HS                      3                  Search candidate validation
-30/4/2024           HS                      3                   Edit candidate details
+29/4/2024   | HS                      | 3        | Search candidate validation
+30/4/2024   | HS                      | 3        | Edit candidate details
 29/4/2024   | HS                      | 3        | Search candidate validation
 29/4/2024   | Harshini C              | 3        | View Candidates applied in
 -------------------------------------------------------------------------------------------------------
 */
 
 const express = require('express')
-const { addCandidate, searchCandidate, upload, editCandidate, viewCandidate, viewCandidatebydate } = require('../controllers/candidateController')
+const { addCandidate, searchCandidate, viewCandidateByYearMonth, upload, editCandidate, viewCandidate } = require('../controllers/candidateController')
 
 const router = express.Router()
 
@@ -26,8 +26,8 @@ router.post('/add-candidate', upload.single('resume'), addCandidate)
 // view candidate by field
 router.get('/search-candidate', searchCandidate)
 
-// view candidate by date
-router.get('/view-candidate-bydate', viewCandidatebydate)
+// view candidate by year and month
+router.get('/view-candidate', viewCandidateByYearMonth)
 
 // view single candidate
 router.get('/view-candidate/:id', viewCandidate) 
