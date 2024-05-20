ead442d5df57a409cb2ba150aa2744e013e722ce
HS-Fecund <160869061+HS-Fecund@users.noreply.github.com>
2024-05-20 11:12:25 +0530

    Update server.js

--- hrportal/server/server.js
+++ hrportal/server/server.js
@@ -30,7 +30,7 @@ const app = express()
     
 
 //middleware
-app.use(cors({ origin: client_port , credentials: true}))
+app.use(cors({ origin: "https://deploy-hiringportal.vercel.app" , methods: ["POST","GET"], credentials: true}))
 app.use(express.json())
 
 app.use((req, res, next) => {
